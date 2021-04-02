c4add82a621dbad3bde60224b55d2b22888297a8
Igor Frey <igorfreyy97@gmail.com>
2021-04-01 22:30:04 -0300

    NLW 04 Enviando emails

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,18 @@
 import {Router} from 'express';
 import {UserController} from "./controllers/UserController";
 import {SurveysController} from "./controllers/SurveysController";
+import { SendEmailController } from './controllers/SendMailController';
 
 const router = Router();
 
 const userController = new UserController();
 const surveysController = new SurveysController();
+const sendMailController = new SendEmailController();
 
 router.post("/users", userController.create);
 router.post("/surveys", surveysController.create);
 router.get("/show", surveysController.show);
+
+router.post("/sendMail", sendMailController.execute);
 
 export {router};
